fc8c099f0fdb5d921fca2cb21292396e433cb844
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2023-03-21 15:13:13 +0100

    update client

--- frontend/src/lib/sanity/client.js
+++ frontend/src/lib/sanity/client.js
@@ -4,6 +4,7 @@ export const client = createClient({
     projectId: '2kxd9hp8',
     dataset: 'production',
     useCdn: true,
+    //se sanityprosjekt-> vision-> API vision
     apiVersion: '2021-10-21',
 
 })
